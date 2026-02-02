52dd7de80fffb80f13a2db34eef1a7cbb84c2897
Jian Yang PC <jyang173173@gmail.com>
2026-02-02 12:46:55 -0600

    init commit

---
+++ src/main/java/com/jian/store_mosh/StoreMoshApplication.java
@@ -0,0 +1,13 @@
+package com.jian.store_mosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMoshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreMoshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jian/store_mosh/StoreMoshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jian.store_mosh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMoshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
